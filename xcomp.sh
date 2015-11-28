ddeb681b3b53acc6b7f3a19bf8f659b4d8b6937f
geekan <geekan@foxmail.com>
2015-11-28 18:01:05 +0000

    implement compress rule.

--- xcomp.sh
+++ xcomp.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # function Extract for common file formats
 
- if [ -z "$1" ]; then
+ if [ -z "$1" ] || [ -z "$2" ]; then
     # display usage if no parameters given
-    echo "Usage: xcomp <path/file_name> <compressed_filename>.<extension>"
+    echo "Usage: xcomp <path|file_name> <compressed_filename>.<extension>"
     echo "  available extensions: <zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
  else
-    if [ -f "$1" ] ; then
-        NAME=${1%.*}
+    if [ -f "$1" ] || [ -d "$1" ]; then
+        #NAME=${1%.*}
         #mkdir $NAME && cd $NAME
-        case "$1" in
-          *.tar.bz2)   tar xvjf ./"$1"    ;;
-          *.tar.gz)    tar xvzf ./"$1"    ;;
-          *.tar.xz)    tar xvJf ./"$1"    ;;
-          *.lzma)      unlzma ./"$1"      ;;
-          *.bz2)       bunzip2 ./"$1"     ;;
-          *.rar)       unrar x -ad ./"$1" ;;
-          *.gz)        gunzip ./"$1"      ;;
-          *.tar)       tar xvf ./"$1"     ;;
-          *.tbz2)      tar xvjf ./"$1"    ;;
-          *.tgz)       tar xvzf ./"$1"    ;;
-          *.zip)       unzip ./"$1"       ;;
-          *.Z)         uncompress ./"$1"  ;;
-          *.7z)        7z x ./"$1"        ;;
-          *.xz)        unxz ./"$1"        ;;
-          *.exe)       cabextract ./"$1"  ;;
+        case "$2" in
+          *.tar.bz2)   tar cvjf ./"$1.$2" ./"$1"    ;;
+          *.tar.gz)    tar cvzf ./"$1.$2" ./"$1"    ;;
+          *.tar.xz)    tar cvJf ./"$1.$2" ./"$1"    ;;
+          #*.lzma)      unlzma ./"$1"      ;;
+          *.bz2)       bzip2 -k ./"$1"    ;;
+          #*.rar)       unrar x -ad ./"$1" ;;
+          #*.gz)        gunzip ./"$1"      ;;
+          *.tar)       tar cvf ./"$1.$2" ./"$1"     ;;
+          *.tbz2)      tar cvjf ./"$1.$2" ./"$1"    ;;
+          *.tgz)       tar cvzf ./"$1.$2" ./"$1"    ;;
+          *.zip)       zip -r ./"$1.zip" ./"$1"       ;;
+          #*.Z)         uncompress ./"$1"  ;;
+          #*.7z)        7z x ./"$1"        ;;
+          #*.xz)        unxz ./"$1"        ;;
+          #*.exe)       cabextract ./"$1"  ;;
           *)           echo "extract: '$1' - unknown archive method" ;;
         esac
     else
